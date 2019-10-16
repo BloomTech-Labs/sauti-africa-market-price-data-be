be757978f534ed9c0b4895c4baaf1dcfe7fdf5fc
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-10-16 15:13:57 -0500

    Testing queries

--- api/server.js
+++ api/server.js
@@ -12,20 +12,65 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send('working in my test server');
-  });
-function getThings(){
-    return DBSt('platform_market_prices').orderBy('date').limit(10)
+});
 
+function getSautiData(query){
+
+    let queryOperation = DBSt('platform_market_prices');
+    if (query.c && !Array.isArray(query.c)) {
+        queryOperation = queryOperation.where('country', query.c);
+    } else {
+        queryOperation = queryOperation.whereIn('country', query.c);
+        console.log(query.c);
+    }
+    if (query.market) {
+        queryOperation = queryOperation.whereIn('country','like', `%${query.market}%`);
+        console.log(query.market);
+    }
+    if (query.product_agg) {
+        queryOperation = queryOperation.where('product_agg', 'like', `%${query.product_agg}%`);
+    }
+    if (query.product) {
+        queryOperation = queryOperation.where('product', 'like', `%${query.product}%`);
+    }
+    queryOperation = queryOperation.orderBy('date').limit(query.limit || 15);
+    return queryOperation;
 }
+
 server.get('/sauti', (req, res) => {
-    getThings().then(records => {
+    getSautiData(req.query).then(records => {
         res.status(200).json(records)
     })
     .catch(error => {
         console.log(error)
         res.status(500).send(error.message)
     })
-    
 })
+
+// function getThings(query){
+
+//     let queryOperation = DBSt('platform_market_prices');
+//     if (query.country) {
+//         queryOperation = queryOperation.where('country', query.country);
+//     }
+//     if (query.product_agg) {
+//         queryOperation = queryOperation.where('product_agg', 'like', `%${query.product_agg}%`);
+//     }
+//     if (query.product) {
+//         queryOperation = queryOperation.where('product', query.product);
+//     }
+//         queryOperation = queryOperation.orderBy('date').limit(query.limit || 10);
+//         return queryOperation;
+// }
+
+// server.get('/sauti', (req, res) => {
+//     getThings(req.query).then(records => {
+//         res.status(200).json(records)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//         res.status(500).send(error.message)
+//     })
+// })
 
 module.exports = server;
