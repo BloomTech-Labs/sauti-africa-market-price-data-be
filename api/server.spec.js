4ee66cdb4d720165fc6c1295f321a83bfd693072
alexisjcarr <acarr59@gmail.com>
2019-10-17 17:43:20 -0500

    ✅ Updates staging with Angela's travis fixes

---
+++ api/server.spec.js
@@ -0,0 +1,10 @@
+const request = require("supertest");
+const app = require("./server");
+
+describe("server", () => {
+  it("returns 200", async () => {
+    const res = await request(app).get("/");
+
+    expect(res.statusCode).toEqual(200);
+  });
+});
