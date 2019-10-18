30c8b1bf32acda911f14a508c7b73765d5fac89c
alexisjcarr <acarr59@gmail.com>
2019-10-18 17:36:08 -0500

    🎨 Runs prettier

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const helmet = require("helmet")
+
 const DBSt = require("../database/dbSTConfig")
 const apikeyRoute = require("./testRoute.js")
 
