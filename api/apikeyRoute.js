2fc89cc4f87d0bd54a011e1076d7438a0fb76852
alexisjcarr <acarr59@gmail.com>
2019-10-18 18:51:17 -0500

    🗃 Adds logic to add apikey to key db

--- api/apikeyRoute.js
+++ api/apikeyRoute.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const db = require("../database/dbConfig.js")
 const jwt = require("express-jwt")
 const jwtAuthz = require("express-jwt-authz")
 const jwks = require("jwks-rsa")
 const uuidAPIKey = require("uuid-apikey")
+
+const db = require("../api-key/dbConfig")
 
 const jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
@@ -13,15 +14,15 @@ const jwtCheck = jwt({
     jwksRequestsPerMinute: 5,
     jwksUri: "http://sauti-africa-market-prices.auth0.com/.well-known/jwks.json"
   }),
-
   audience: "https://sauti-africa-market-prices.auth0.com/api/v2/",
   issuer: "http://sauti-africa-market-prices.auth0.com/",
   algorithms: ["RS256"]
 })
 
-router.get("/private", jwtCheck, (req, res) => {
-  const apiKey = uuidAPIKey.create()
-  res.status(200).json({ key: apiKey.apiKey })
+router.get("/private", jwtCheck, async (req, res) => {
+  const key = uuidAPIKey.create()
+  await db("api-key").insert({ key })
+  res.status(200).json({ key: key.apiKey })
 })
 
 module.exports = router
