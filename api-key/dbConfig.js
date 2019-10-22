99694adcb4c70634c1a2a037564e558f64173b23
Shawn McManus <xsvfan76@gmail.com>
2019-10-22 11:20:06 -0600

    added-secrets-to-dbConfig-in-API-key

--- api-key/dbConfig.js
+++ api-key/dbConfig.js
@@ -1,9 +1,11 @@
 const knex = require("knex")
+const secrets = require("../config/secrets.js")
 
 const config = require("../knexfile.js")
 
+const environment = secrets.environment
 // we must select the development object from our knexfile
-const db = knex(config.development) //TODO: change this to be dynamic later
+const db = knex(config.environment) //TODO: change this to be dynamic later
 
 // export for use in codebase
 module.exports = db
