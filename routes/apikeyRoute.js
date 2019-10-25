da3012b652660b5e27287737922061a75b5c8c2c
Shawn McManus <xsvfan76@gmail.com>
2019-10-24 19:02:26 -0600

    Created API key and hashed them.
    Then sent plain API key to user

--- routes/apikeyRoute.js
+++ routes/apikeyRoute.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const jwt = require('express-jwt')
 const jwks = require('jwks-rsa')
 const uuidAPIKey = require('uuid-apikey')
+const bcrypt = require('bcryptjs')
 
 const db = require('../api-key/dbConfig')
 
@@ -14,29 +15,60 @@ const jwtCheck = jwt({
     jwksUri:
       'https://sauti-africa-market-prices.auth0.com/.well-known/jwks.json'
   }),
-  audience: "https://sauti-africa-market-prices.auth0.com/api/v2/",
-  issuer: "https://sauti-africa-market-prices.auth0.com/",
-  algorithms: ["RS256"]
-});
+  audience: 'https://sauti-africa-market-prices.auth0.com/api/v2/',
+  issuer: 'https://sauti-africa-market-prices.auth0.com/',
+  algorithms: ['RS256']
+})
 
-router.post("/private", jwtCheck, async (req, res) => {
-  const key = uuidAPIKey.create();
-  const { id } = req.body;
-  console.log({ ID: id });
-  const user = await db("apiKeys")
+router.post('/private', jwtCheck, async (req, res) => {
+  const key = uuidAPIKey.create()
+  const { id } = req.body
+
+  const user = await db('apiKeys')
     .where({ user_id: id })
-    .first();
-  console.log({ here: user });
-  if (user) res.status(200).json({ existed: true, key: user.key });
-  else {
-    try {
-      console.log("no user");
-      await db("apiKeys").insert({ key: key.apiKey, user_id: id });
-      res.status(200).json({ existed: false, key: key.apiKey });
-    } catch (error) {
-      console.log(error);
-    }
-  }
-});
+    .first()
 
-module.exports = router;
+  bcrypt.hash(key.apiKey, 10, async function(err, hash) {
+    if (hash) {
+      if (user) {
+        await db('apiKeys')
+          .where({ user_id: id })
+          .update({ key: hash })
+        res.status(200).json({ existed: true, key: key.apiKey })
+      } else {
+        try {
+          console.log('no user')
+          await db('apiKeys').insert({ key: hash, user_id: id })
+          res.status(200).json({ existed: false, key: key.apiKey })
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    }
+    if (err) {
+      res.status(500).json({ message: 'Key could not be hashed' })
+    }
+  })
+
+  console.log({ ID: id })
+  // const user = await db('apiKeys')
+  //   .where({ user_id: id })
+  //   .first()
+  console.log({ here: user })
+  // if (user) {
+  //   await db('apiKeys')
+  //     .where({ user_id: id })
+  //     .update({ key: hash })
+  //   res.status(200).json({ existed: true, key: key.apiKey })
+  // } else {
+  //   try {
+  //     console.log('no user')
+  //     await db('apiKeys').insert({ key: hash, user_id: id })
+  //     res.status(200).json({ existed: false, key: key.apiKey })
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+})
+
+module.exports = router
