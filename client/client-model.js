81cc6cb8a14491e7932b1bbb6b328e66a52bbd2e
Angela La Salle <angela.lasalle91@gmail.com>
2019-11-08 15:05:58 -0500

    client pagination complete

--- client/client-model.js
+++ client/client-model.js
@@ -1,5 +1,4 @@
 const DBSt = require("../database/dbSTConfig");
-const moment = require("moment");
 
 module.exports = {
   getSautiDataClient,
@@ -15,9 +14,6 @@ async function getSautiDataClient(query) {
 
   let entries;
   let totalCount;
-
-  if (query.next && query.prev)
-    throw { message: "Cannot use next and prev at the same time!" };
 
   if (query.next) {
     const cursorArray = query.next.split("_");
@@ -97,91 +93,7 @@ async function getSautiDataClient(query) {
       .orderBy("date", "desc")
       .orderBy("id", "desc")
       .limit(51);
-  } else if (query.prev) {
-    const cursorArray = query.prev.split("_");
-    const prevDate = new Date(cursorArray[0]);
-    const prevId = cursorArray[1];
-    const cursorLimit = query.limit.split("_");
-    const limit = cursorLimit[1];
-    let queryOperation = DBSt("platform_market_prices2");
-
-    // If user wants data from specific country/countries
-    if (query.c && !Array.isArray(query.c)) {
-      queryOperation = queryOperation.whereIn("country", [query.c]);
-    } else if (query.c && Array.isArray(query.c)) {
-      queryOperation = queryOperation.whereIn("country", query.c);
-    }
-
-    // If user wants data from specific markets
-    if (query.m && !Array.isArray(query.m)) {
-      queryOperation = queryOperation.whereIn("market", [query.m]);
-    } else if (query.m && Array.isArray(query.m)) {
-      queryOperation = queryOperation.whereIn("market", query.m);
-    }
-
-    //if user wants data from spcific product categories
-    if (query.pcat && !Array.isArray(query.pcat)) {
-      //pcat = product category (product_cat) -> General
-      queryOperation = queryOperation.whereIn("product_cat", [query.pcat]);
-    } else if (query.pcat && Array.isArray(query.pcat)) {
-      queryOperation = queryOperation.whereIn("product_cat", query.pcat);
-    }
-
-    //if user wnats data from product subcategory
-    if (query.pagg && !Array.isArray(query.pagg)) {
-      //pagg = product_agg -> product type
-      queryOperation = queryOperation.whereIn("product_agg", [query.pagg]);
-    } else if (query.pagg && Array.isArray(query.pagg)) {
-      queryOperation = queryOperation.whereIn("product_agg", query.pagg);
-    }
-
-    //if user wants data of specific products
-    if (query.p && !Array.isArray(query.p)) {
-      //p = product -> Specific product
-      queryOperation = queryOperation.whereIn("product", [query.p]);
-    } else if (query.p && Array.isArray(query.p)) {
-      queryOperation = queryOperation.whereIn("product", query.p);
-    }
-
-    queryOperation = queryOperation.select(
-      "id",
-      "country",
-      "market",
-      "source",
-      "product_cat",
-      "product_agg",
-      "product",
-      "retail",
-      "wholesale",
-      "currency",
-      "unit",
-      "date",
-      "udate"
-    );
-
-    if (startDate && endDate) {
-      queryOperation = queryOperation.andWhereBetween("date", [
-        startDate,
-        endDate
-      ]);
-    }
-
-    entries = await queryOperation
-      .where("active", (query.a = 1))
-      .where(function() {
-        this.whereRaw("date > ?", [prevDate])
-          .andWhereRaw("id > ?", [prevId])
-          .andWhereRaw("id < ?", [limit]);
-      })
-      .orWhere(function() {
-        this.whereRaw("date = ?", [prevDate])
-          .andWhereRaw("id > ?", [prevDate])
-          .andWhereRaw("id < ?", [limit]);
-      })
-      .orderBy("date", "desc")
-      .orderBy("id", "desc")
-      .limit(50);
-  } else if (!query.next && !query.prev) {
+  } else {
     // If user wants data from specific country/countries
     let queryOperation = DBSt("platform_market_prices2");
     if (query.c && !Array.isArray(query.c)) {
@@ -250,24 +162,24 @@ async function getSautiDataClient(query) {
     entries = await queryOperation
       .orderBy("date", "desc")
       .orderBy("id", "desc")
-      .limit(50);
+      .limit(51);
   }
 
-  const firstEntry = entries[0];
   const lastEntry = entries[entries.length - 1];
-  entries.length
+
+  entries.length ? (next = `${lastEntry.date}_${lastEntry.id}`) : (next = null);
+  const entriesOffset = entries.splice(0, 50);
+
+  const firstEntry = entriesOffset[0];
+  entriesOffset.length
     ? (prev = `${firstEntry.date}_${firstEntry.id}`)
     : (prev = null);
-  entries.length ? (next = `${lastEntry.date}_${lastEntry.id}`) : (next = null);
-  const entriesOffset = entries.splice(0, 49);
 
   return {
     records: entriesOffset,
     next: next,
     prev: prev,
-    count: totalCount,
-    first: firstEntry,
-    last: lastEntry
+    count: totalCount
   };
 }
 function marketList() {
