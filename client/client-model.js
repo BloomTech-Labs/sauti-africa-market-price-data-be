ee68308ec6a86710fca1ec8588fe6c7b9cdb9044
Angela La Salle <angela.lasalle91@gmail.com>
2019-11-20 22:25:33 -0500

    code commenting for cursor pagination

--- client/client-model.js
+++ client/client-model.js
@@ -1,4 +1,4 @@
-const DBSt = require('../database/dbSTConfig')
+const DBSt = require("../database/dbSTConfig");
 
 module.exports = {
   getSautiDataClient,
@@ -7,7 +7,7 @@ module.exports = {
   getPlay,
   getProductPriceRangePlay,
   getPMPlay
-}
+};
 
 // Grid table functions
 
@@ -15,212 +15,219 @@ module.exports = {
 // Flexible by allowing user to select whichever query they want.
 
 async function getSautiDataClient(query, csvLimit) {
-  let { startDate, endDate } = query
+  let { startDate, endDate } = query;
 
-  let entries
-  let totalCount
-
+  let entries;
+  let totalCount;
+  // if next value is passed in (if user is changing the page) - the next value will be a concatenation
+  // of the date and id of the top result in the next page.
   if (query.next) {
-    const cursorArray = query.next.split('_')
-    const nextDate = new Date(cursorArray[0])
-    const nextId = cursorArray[1]
-    let queryOperation = DBSt('platform_market_prices2')
+    //get date and id from the next value (these are used to sort the results to get pages)
+    const cursorArray = query.next.split("_");
+    const nextDate = new Date(cursorArray[0]);
+    const nextId = cursorArray[1];
+    let queryOperation = DBSt("platform_market_prices2");
 
     // If user wants data from specific country/countries
     if (query.c && !Array.isArray(query.c)) {
-      queryOperation = queryOperation.whereIn('country', [query.c])
+      queryOperation = queryOperation.whereIn("country", [query.c]);
     } else if (query.c && Array.isArray(query.c)) {
-      queryOperation = queryOperation.whereIn('country', query.c)
+      queryOperation = queryOperation.whereIn("country", query.c);
     }
 
     // If user wants data from specific markets
     if (query.m && !Array.isArray(query.m)) {
-      queryOperation = queryOperation.whereIn('market', [query.m])
+      queryOperation = queryOperation.whereIn("market", [query.m]);
     } else if (query.m && Array.isArray(query.m)) {
-      queryOperation = queryOperation.whereIn('market', query.m)
+      queryOperation = queryOperation.whereIn("market", query.m);
     }
 
     //if user wants data from spcific product categories
     if (query.pcat && !Array.isArray(query.pcat)) {
       //pcat = product category (product_cat) -> General
-      queryOperation = queryOperation.whereIn('product_cat', [query.pcat])
+      queryOperation = queryOperation.whereIn("product_cat", [query.pcat]);
     } else if (query.pcat && Array.isArray(query.pcat)) {
-      queryOperation = queryOperation.whereIn('product_cat', query.pcat)
+      queryOperation = queryOperation.whereIn("product_cat", query.pcat);
     }
 
     //if user wnats data from product subcategory
     if (query.pagg && !Array.isArray(query.pagg)) {
       //pagg = product_agg -> product type
-      queryOperation = queryOperation.whereIn('product_agg', [query.pagg])
+      queryOperation = queryOperation.whereIn("product_agg", [query.pagg]);
     } else if (query.pagg && Array.isArray(query.pagg)) {
-      queryOperation = queryOperation.whereIn('product_agg', query.pagg)
+      queryOperation = queryOperation.whereIn("product_agg", query.pagg);
     }
 
     //if user wants data of specific products
     if (query.p && !Array.isArray(query.p)) {
       //p = product -> Specific product
-      queryOperation = queryOperation.whereIn('product', [query.p])
+      queryOperation = queryOperation.whereIn("product", [query.p]);
     } else if (query.p && Array.isArray(query.p)) {
-      queryOperation = queryOperation.whereIn('product', query.p)
+      queryOperation = queryOperation.whereIn("product", query.p);
     }
 
     queryOperation = queryOperation.select(
-      'id',
-      'country',
-      'market',
-      'source',
-      'product_cat',
-      'product_agg',
-      'product',
-      'retail',
-      'wholesale',
-      'currency',
-      'unit',
-      'date',
-      'udate'
-    )
+      "id",
+      "country",
+      "market",
+      "source",
+      "product_cat",
+      "product_agg",
+      "product",
+      "retail",
+      "wholesale",
+      "currency",
+      "unit",
+      "date",
+      "udate"
+    );
 
     if (startDate && endDate) {
-      queryOperation = queryOperation.andWhereBetween('date', [
+      queryOperation = queryOperation.andWhereBetween("date", [
         startDate,
         endDate
-      ])
+      ]);
     }
 
+    // of all of the results that match, find the ones whose date is less than the date in the next value,
+    // or, in the case of identical dates, grab the ones whose date is identical and id is less than or equal
+    // than the date and id in the passed in next value.
     entries = await queryOperation
       .where(function() {
-        this.whereRaw('date < ?', [nextDate]).orWhere(function() {
-          this.whereRaw('date = ?', [nextDate]).andWhereRaw('id <= ?', [nextId])
-        })
-        // .andWhereRaw("id <= ?", [nextId]);
+        this.whereRaw("date < ?", [nextDate]).orWhere(function() {
+          this.whereRaw("date = ?", [nextDate]).andWhereRaw("id <= ?", [
+            nextId
+          ]);
+        });
       })
 
-
-      .where('active', (query.a = 1))
-      .orderBy('date', 'desc')
-      .orderBy('id', 'desc')
-      .limit(csvLimit || 31)
-
+      // grab active ones, sort by date and id, return up to full page plus 1 (top of next page - used as next value)
+      .where("active", (query.a = 1))
+      .orderBy("date", "desc")
+      .orderBy("id", "desc")
+      .limit(csvLimit || 31);
   } else {
     // If user wants data from specific country/countries
-    let queryOperation = DBSt('platform_market_prices2')
+    let queryOperation = DBSt("platform_market_prices2");
     if (query.c && !Array.isArray(query.c)) {
-      queryOperation = queryOperation.whereIn('country', [query.c])
+      queryOperation = queryOperation.whereIn("country", [query.c]);
     } else if (query.c && Array.isArray(query.c)) {
-      queryOperation = queryOperation.whereIn('country', query.c)
+      queryOperation = queryOperation.whereIn("country", query.c);
     }
 
     // If user wants data from specific markets
     if (query.m && !Array.isArray(query.m)) {
-      queryOperation = queryOperation.whereIn('market', [query.m])
+      queryOperation = queryOperation.whereIn("market", [query.m]);
     } else if (query.m && Array.isArray(query.m)) {
-      queryOperation = queryOperation.whereIn('market', query.m)
+      queryOperation = queryOperation.whereIn("market", query.m);
     }
 
     //if user wants data from spcific product categories
     if (query.pcat && !Array.isArray(query.pcat)) {
       //pcat = product category (product_cat) -> General
-      queryOperation = queryOperation.whereIn('product_cat', [query.pcat])
+      queryOperation = queryOperation.whereIn("product_cat", [query.pcat]);
     } else if (query.pcat && Array.isArray(query.pcat)) {
-      queryOperation = queryOperation.whereIn('product_cat', query.pcat)
+      queryOperation = queryOperation.whereIn("product_cat", query.pcat);
     }
 
     //if user wnats data from product subcategory
     if (query.pagg && !Array.isArray(query.pagg)) {
       //pagg = product_agg -> product type
-      queryOperation = queryOperation.whereIn('product_agg', [query.pagg])
+      queryOperation = queryOperation.whereIn("product_agg", [query.pagg]);
     } else if (query.pagg && Array.isArray(query.pagg)) {
-      queryOperation = queryOperation.whereIn('product_agg', query.pagg)
+      queryOperation = queryOperation.whereIn("product_agg", query.pagg);
     }
 
     //if user wants data of specific products
     if (query.p && !Array.isArray(query.p)) {
       //p = product -> Specific product
-      queryOperation = queryOperation.whereIn('product', [query.p])
+      queryOperation = queryOperation.whereIn("product", [query.p]);
     } else if (query.p && Array.isArray(query.p)) {
-      queryOperation = queryOperation.whereIn('product', query.p)
+      queryOperation = queryOperation.whereIn("product", query.p);
     }
 
     queryOperation = queryOperation.select(
-      'id',
-      'country',
-      'market',
-      'source',
-      'product_cat',
-      'product_agg',
-      'product',
-      'retail',
-      'wholesale',
-      'currency',
-      'unit',
-      'date',
-      'udate'
-    )
+      "id",
+      "country",
+      "market",
+      "source",
+      "product_cat",
+      "product_agg",
+      "product",
+      "retail",
+      "wholesale",
+      "currency",
+      "unit",
+      "date",
+      "udate"
+    );
 
     if (startDate && endDate) {
-      queryOperation = queryOperation.andWhereBetween('date', [
+      queryOperation = queryOperation.andWhereBetween("date", [
         startDate,
         endDate
-      ])
+      ]);
     }
-    totalCount = await queryOperation.clone().count()
+
+    // total count of results for entire query
+    totalCount = await queryOperation.clone().count();
     entries = await queryOperation
 
-      .where('active', (query.a = 1))
-      .orderBy('date', 'desc')
-      .orderBy('id', 'desc')
-      .limit(csvLimit || 31)
-
+      .where("active", (query.a = 1))
+      .orderBy("date", "desc")
+      .orderBy("id", "desc")
+      .limit(csvLimit || 31);
   }
+  // last entry of entries is the top entry of the next page
+  const lastEntry = entries[entries.length - 1];
+  //concatenate the date and id of the lastEntry
+  entries.length ? (next = `${lastEntry.date}_${lastEntry.id}`) : (next = null);
+  //grab all but the last lastEntry (this will be the page you return)
+  const entriesOffset = entries.splice(0, csvLimit || 30);
 
-  const lastEntry = entries[entries.length - 1]
-
-  entries.length ? (next = `${lastEntry.date}_${lastEntry.id}`) : (next = null)
-  const entriesOffset = entries.splice(0, csvLimit || 30)
-
-  const firstEntry = entriesOffset[0]
+  const firstEntry = entriesOffset[0];
   entriesOffset.length
     ? (prev = `${firstEntry.date}_${firstEntry.id}`)
-    : (prev = null)
+    : (prev = null);
 
   return {
     records: entriesOffset,
     next: next,
     prev: prev,
     count: totalCount
-  }
+  };
 }
 function marketList() {
-  return DBSt('platform_market_prices2')
-    .distinct('market')
-    .orderBy('market')
+  return DBSt("platform_market_prices2")
+    .distinct("market")
+    .orderBy("market");
 }
 function countryList() {
-  return DBSt('platform_market_prices2')
-    .distinct('country')
-    .orderBy('country')
+  return DBSt("platform_market_prices2")
+    .distinct("country")
+    .orderBy("country");
 }
 function productList() {
-  return DBSt('platform_market_prices2')
-    .distinct('product')
-    .orderBy('product')
+  return DBSt("platform_market_prices2")
+    .distinct("product")
+    .orderBy("product");
 }
 function pcatList() {
-  return DBSt('platform_market_prices2')
-    .distinct('product_cat')
-    .orderBy('product_cat')
+  return DBSt("platform_market_prices2")
+    .distinct("product_cat")
+    .orderBy("product_cat");
 }
 function paggList() {
-  return DBSt('platform_market_prices2')
-    .distinct('product_agg')
-    .orderBy('product_agg')
+  return DBSt("platform_market_prices2")
+    .distinct("product_agg")
+    .orderBy("product_agg");
 }
 function mcpList() {
-  const marketQuery = marketList()
-  const countryQuery = countryList()
-  const productQuery = productList()
-  const pcatQuery = pcatList()
-  const paggQuery = paggList()
+  const marketQuery = marketList();
+  const countryQuery = countryList();
+  const productQuery = productList();
+  const pcatQuery = pcatList();
+  const paggQuery = paggList();
   return Promise.all([
     marketQuery,
     countryQuery,
@@ -228,36 +235,36 @@ function mcpList() {
     pcatQuery,
     paggQuery
   ]).then(([markets, country, product, category, aggregator]) => {
-    let total = {}
-    total.countries = country
-    total.products = product
-    total.markets = markets
-    total.categories = category
-    total.aggregators = aggregator
-    return total
-  })
+    let total = {};
+    total.countries = country;
+    total.products = product;
+    total.markets = markets;
+    total.categories = category;
+    total.aggregators = aggregator;
+    return total;
+  });
 }
 
 function getListsOfThings(query, selector) {
-  let queryOperation = DBSt('platform_market_prices2')
+  let queryOperation = DBSt("platform_market_prices2");
   if (query === undefined) {
-    query = 'market'
+    query = "market";
   }
   switch (query.toLowerCase()) {
-    case 'market':
-      return queryOperation.distinct('market').orderBy('market')
-    case 'country':
-      return queryOperation.distinct('country').orderBy('country')
-    case 'source':
-      return queryOperation.distinct('source').orderBy('source')
-    case 'product':
-      return queryOperation.distinct('product').orderBy('product')
-    case 'category':
-      return queryOperation.distinct('product_cat').orderBy('product_cat')
-    case 'aggregator':
-      return queryOperation.distinct('product_agg').orderBy('product_agg')
+    case "market":
+      return queryOperation.distinct("market").orderBy("market");
+    case "country":
+      return queryOperation.distinct("country").orderBy("country");
+    case "source":
+      return queryOperation.distinct("source").orderBy("source");
+    case "product":
+      return queryOperation.distinct("product").orderBy("product");
+    case "category":
+      return queryOperation.distinct("product_cat").orderBy("product_cat");
+    case "aggregator":
+      return queryOperation.distinct("product_agg").orderBy("product_agg");
     default:
-      return queryOperation.distinct('market').orderBy('market')
+      return queryOperation.distinct("market").orderBy("market");
   }
 }
 // End of Grid table functions
@@ -265,95 +272,93 @@ function getListsOfThings(query, selector) {
 // Playground functions
 //filter playground function//
 function getPlay(query) {
-  let queryOperation = DBSt('platform_market_prices2')
+  let queryOperation = DBSt("platform_market_prices2");
 
   // If user wants data from specific country/countries
   if (query.c && !Array.isArray(query.c)) {
-    queryOperation = queryOperation.whereIn('country', [query.c])
+    queryOperation = queryOperation.whereIn("country", [query.c]);
   } else if (query.c && Array.isArray(query.c)) {
-    queryOperation = queryOperation.whereIn('country', query.c)
+    queryOperation = queryOperation.whereIn("country", query.c);
   }
 
   // If user wants data from specific markets
   if (query.market && !Array.isArray(query.market)) {
-    queryOperation = queryOperation.whereIn('market', [query.market])
+    queryOperation = queryOperation.whereIn("market", [query.market]);
   } else if (query.market && Array.isArray(query.market)) {
-    queryOperation = queryOperation.whereIn('market', query.market)
+    queryOperation = queryOperation.whereIn("market", query.market);
   }
 
   //if user wants data from spcific product categories
   if (query.pcat && !Array.isArray(query.pcat)) {
     //pcat = product category (product_cat) -> General
-    queryOperation = queryOperation.whereIn('product_cat', [query.pcat])
+    queryOperation = queryOperation.whereIn("product_cat", [query.pcat]);
   } else if (query.pcat && Array.isArray(query.pcat)) {
-    queryOperation = queryOperation.whereIn('product_cat', query.pcat)
+    queryOperation = queryOperation.whereIn("product_cat", query.pcat);
   }
 
   //if user wnats data from product subcategory
   if (query.pagg && !Array.isArray(query.pagg)) {
     //pagg = product_agg -> product type
-    queryOperation = queryOperation.whereIn('product_agg', [query.pagg])
+    queryOperation = queryOperation.whereIn("product_agg", [query.pagg]);
   } else if (query.pagg && Array.isArray(query.pagg)) {
-    queryOperation = queryOperation.whereIn('product_agg', query.pagg)
+    queryOperation = queryOperation.whereIn("product_agg", query.pagg);
   }
 
   //if user wants data of specific products
   if (query.p && !Array.isArray(query.p)) {
     //p = product -> Specific product
-    queryOperation = queryOperation.whereIn('product', [query.p])
+    queryOperation = queryOperation.whereIn("product", [query.p]);
   } else if (query.p && Array.isArray(query.p)) {
-    queryOperation = queryOperation.whereIn('product', query.p)
+    queryOperation = queryOperation.whereIn("product", query.p);
   }
 
   return queryOperation
     .select(
-      'country',
-      'market',
-      'source',
-      'product_cat',
-      'product_agg',
-      'product',
-      'retail',
-      'wholesale',
-      'currency',
-      'unit',
-      'date',
-      'udate'
+      "country",
+      "market",
+      "source",
+      "product_cat",
+      "product_agg",
+      "product",
+      "retail",
+      "wholesale",
+      "currency",
+      "unit",
+      "date",
+      "udate"
     )
-    .orderBy('date', 'desc')
-    .where('active', (query.a = 1))
-    .limit(1)
+    .orderBy("date", "desc")
+    .where("active", (query.a = 1))
+    .limit(1);
 }
 
-
 function getProductPriceRangePlay({ product, startDate, endDate }) {
-
-  return DBSt('platform_market_prices2')
-    .select('*')
-    .where('product', product)
-    .andWhereBetween('date', [startDate, endDate])
-    .limit(1)
+  return DBSt("platform_market_prices2")
+    .select("*")
+    .where("product", product)
+    .andWhereBetween("date", [startDate, endDate])
+    .limit(1);
 }
 
 function getPMPlay(query) {
-  const { product, market } = query
-  let queryOperation = DBSt('platform_market_prices2')
+  const { product, market } = query;
+  let queryOperation = DBSt("platform_market_prices2");
   return queryOperation
     .select(
-      'market',
-      'source',
-      'country',
-      'currency',
-      'product',
-      'retail',
-      'wholesale',
-      'date',
-      'udate'
+      "market",
+      "source",
+      "country",
+      "currency",
+      "product",
+      "retail",
+      "wholesale",
+      "date",
+      "udate"
     )
-    .where('product', `${product}`)
-    .andWhere('market', `${market}`)
-    .orderBy('date', 'desc')
-    .limit(1)
+    .where("product", `${product}`)
+    .andWhere("market", `${market}`)
+    .orderBy("date", "desc")
+    .limit(1);
 }
 
 // End of Playground functions
--- middleware/validate.js
+++ middleware/validate.js
@@ -5,29 +5,31 @@ module.exports = {
   queryCountPage,
   queryCurrency,
   playgroundDR
-}
+};
 
 function queryProduct(req, res, next) {
   if (req.query.product === undefined) {
     res.status(400).json({
       errorMessage: "Please supply the query parameter of 'product' "
-    })
+    });
   } else {
-    next()
+    next();
   }
 }
 
+//default and limit for dynamic results per page limit for developer pagination.
+//default is 30 results per page returned and max is 500 per page.
 function queryCountPage(req, res, next) {
   if (req.query.count > 500) {
-    req.message = 'Each page can have maximum of 500 records'
-    req.query.count = 500
-    next()
+    req.message = "Each page can have maximum of 500 records";
+    req.query.count = 500;
+    next();
   } else if (!req.query.count) {
-    req.message = 'Default count is 30 per page'
-    req.query.count = 30
-    next()
+    req.message = "Default count is 30 per page";
+    req.query.count = 30;
+    next();
   } else {
-    next()
+    next();
   }
 }
 
@@ -35,24 +37,26 @@ function queryProductMarket(req, res, next) {
   if (req.query.product === undefined) {
     res.status(400).json({
       errorMessage: "Please supply the query parameter of 'product' "
-    })
+    });
   } else if (req.query.market === undefined) {
     res
       .status(400)
-      .json({ errorMessage: "Please supply the query parameter of 'market' " })
+      .json({ errorMessage: "Please supply the query parameter of 'market' " });
   } else {
-    next()
+    next();
   }
 }
 function playgroundDR(req, res, next) {
-  if (!req.query.hasOwnProperty('product')) {
-    res.status(400).json({ errorMessage: 'please supply product' })
-  } else if (!req.query.hasOwnProperty('startDate')) {
-    res.status(400).json({ errorMessage: 'please supply startDate=YYYY-MM-DD' })
-  } else if (!req.query.hasOwnProperty('endDate')) {
-    res.status(400).json({ errorMessage: 'please supply endDate=YYYY-MM-DD' })
+  if (!req.query.hasOwnProperty("product")) {
+    res.status(400).json({ errorMessage: "please supply product" });
+  } else if (!req.query.hasOwnProperty("startDate")) {
+    res
+      .status(400)
+      .json({ errorMessage: "please supply startDate=YYYY-MM-DD" });
+  } else if (!req.query.hasOwnProperty("endDate")) {
+    res.status(400).json({ errorMessage: "please supply endDate=YYYY-MM-DD" });
   } else {
-    next()
+    next();
   }
 }
 
@@ -60,43 +64,45 @@ function queryProductDate(req, res, next) {
   if (req.query.product === undefined) {
     res.status(400).json({
       errorMessage: "Please supply the query parameter of 'product' "
-    })
+    });
   } else if (req.query.startDate === undefined) {
     res.status(400).json({
       errorMessage: "Please supply the query parameter of 'startDate' "
-    })
+    });
   } else if (req.query.endDate === undefined) {
     res
       .status(400)
-      .json({ errorMessage: "Please supply the query parameter of 'endDate' " })
+      .json({
+        errorMessage: "Please supply the query parameter of 'endDate' "
+      });
   } else {
-    next()
+    next();
   }
 }
 
 function queryCurrency(req, res, next) {
   const supportedCurrencies = [
-    'MWK',
-    'RWF',
-    'KES',
-    'UGX',
-    'TZS',
-    'CDF',
-    'BIF',
-    'USD'
-  ]
-  const { currency } = req.query
+    "MWK",
+    "RWF",
+    "KES",
+    "UGX",
+    "TZS",
+    "CDF",
+    "BIF",
+    "USD"
+  ];
+  const { currency } = req.query;
 
   if (currency === undefined) {
-    req.currency = 'USD'
-    next()
+    req.currency = "USD";
+    next();
   } else if (!supportedCurrencies.includes(currency.toUpperCase())) {
     res.status(400).json({
       errorMessage: `Parameter 'currency' must be one of:  ${supportedCurrencies}`
-    })
+    });
   } else {
-    req.currency = currency.toUpperCase()
-    next()
+    req.currency = currency.toUpperCase();
+    next();
   }
 }
 
