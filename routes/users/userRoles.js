4bbb9d4a1ebc5a1e366a8b5701abd412d1025554
BaoPham92 <baopham92@yahoo.com>
2019-12-19 01:27:43 -0500

    Functionality for PATCH user data in AUTH0 database by API call

--- routes/users/userRoles.js
+++ routes/users/userRoles.js
@@ -1,14 +1,11 @@
 // TODO: COMPLETE ENDPOINT FUNCTIONALITY. CURRENT STATE IS TESTING DATA FLOW FOR FRONT END
 const router = require('express').Router();
-const { fetchUserSchema } = require('./utils');
+const { fetchUserSchema, assignUserRole } = require('./utils');
 
 // * RETRIEVE THE FULL USER SCHEMA FROM AUTH0
 router.post('/', async (req, res) => fetchUserSchema(req, res))
 
 // * USER INFORMATION TO BE UPDATED FOR ROLE ASSIGNMENT.
-router.put('/:id', (req, res) => {
-    const user = req.body;
-    console.log('SHOULD BE USER DATA OBJECT FROM FRONT END AUTH0 API CLIENT', user)
-})
+router.put('/:id', async (req, res) => assignUserRole(req, res))
 
 module.exports = router;
