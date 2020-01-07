cb5ac7d41c6e073f8095131e81b7a520314c7328
BaoPham92 <baopham92@yahoo.com>
2020-01-06 22:40:00 -0500

    Testing endpoints for retrieving user schema data & updating from auth0

--- api/server.js
+++ api/server.js
@@ -58,6 +58,6 @@ server.get('/sauti', (req, res) => {
 
 // TODO: CLEAN UP TEMP CODE AFTER FINISHED WRITING MIDDLEWARE FOR API
 const roles = require('../middleware/rules/rules-middleware')
-roles();
+// roles();
 
 module.exports = server
